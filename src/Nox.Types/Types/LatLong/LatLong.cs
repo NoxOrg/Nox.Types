85a6c5133e884772e648a58d208d01404e7f7c85
Andre Sharpe <andre.sharpe@gmail.com>
2023-06-14 04:46:39 +0200

    Added LatLong type and tests

---
+++ src/Nox.Types/Types/LatLong/LatLong.cs
@@ -0,0 +1,90 @@
+﻿
+using FluentValidation;
+using FluentValidation.Results;
+using System;
+using System.Globalization;
+
+namespace Nox.Types;
+
+/// <summary>
+/// Represents a Nox <see cref="LatLong"/> type and value object. 
+/// </summary>
+public class LatLong : ValueObject<(double Latitude, double Longitude), LatLong>
+{
+    public LatLong() { Value = (Latitude: 0, Longitude: 0); } // Null Island
+
+    public double Latitude => Value.Latitude;
+
+    public double Longitude => Value.Longitude;
+
+    public static LatLong From(double latitude, double longitude)
+        => From((latitude,longitude));
+
+    /// <summary>
+    /// Validates a <see cref="LatLong"/> object.
+    /// </summary>
+    /// <returns>true if the <see cref="LatLong"/> value is valid geo coordinate.</returns>
+    public override ValidationResult Validate()
+    {
+        var result = base.Validate();
+
+        if (Value.Latitude > 90 || Value.Latitude < -90)
+        {
+            result.Errors.Add(new ValidationFailure(nameof(Value), $"Could not create a Nox LatLong type with latitude {Value.Latitude} as it is not in the range -90 to 90 degrees."));
+        }
+
+        if (Value.Longitude > 180 || Value.Longitude < -180)
+        {
+            result.Errors.Add(new ValidationFailure(nameof(Value), $"Could not create a Nox LatLong type with longitude {Value.Longitude} as it is not in the range -180 to 180 degrees."));
+        }
+
+        return result;
+    }
+
+    public override string ToString()
+    {
+        return $"{Value.Latitude:0.000000} {Value.Longitude:0.000000}";
+    }
+
+    public string ToString(string format)
+    {
+        return format.ToLower() switch
+        {
+            "dms" => ToDmsString(),
+            _ => ToString(),
+        };
+    }
+
+    private string ToDmsString()
+    {
+        var latDirection = Value.Latitude < 0 ? "S" : "N";
+        var lat = ToDegreesMinutesSeconds(Value.Latitude);
+
+        var lonDirection = Value.Longitude < 0 ? "W" : "E";
+        var lon = ToDegreesMinutesSeconds(Value.Longitude);
+
+        return  $"{lat.Degrees}°{lat.Minutes:00}'{lat.Seconds:00}.{lat.Milliseconds:####}\" {latDirection} "+
+                $"{lon.Degrees}°{lon.Minutes:00}'{lon.Seconds:00}.{lon.Milliseconds:####}\" {lonDirection}";
+    }
+
+    private (int Degrees, int Minutes, int Seconds, int Milliseconds) ToDegreesMinutesSeconds(double decimaDegrees)
+    {
+        decimaDegrees = Math.Abs(decimaDegrees);
+        var degrees = (int)Math.Floor(decimaDegrees);
+
+        decimaDegrees -= degrees;
+        double totalSeconds = (int)Math.Floor(3600.0 * decimaDegrees);
+
+        var seconds = (int)Math.Floor(totalSeconds % 60);
+        var minutes = (int)Math.Floor(totalSeconds / 60.0);
+
+        decimaDegrees = (decimaDegrees * 3600.0) - totalSeconds;
+        var milliseconds = (int)(1000.0 * decimaDegrees);
+
+        return (degrees, minutes, seconds, milliseconds);
+    }
+
+}
+
+
+
