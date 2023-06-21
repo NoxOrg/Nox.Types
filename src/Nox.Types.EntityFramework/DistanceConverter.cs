a22b0582e8bf992ca01222a1b24b3ad175940b16
Dejan Bratic <dejan.bratic@igwplc.com>
2023-06-21 09:23:53 +0200

    initial implementation;

---
+++ src/Nox.Types.EntityFramework/DistanceConverter.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Nox.Types.EntityFramework;
+
+public class DistanceToKilometerConverter : ValueConverter<Distance, double>
+{
+    public DistanceToKilometerConverter() : base(area => (double)area.ToKilometers(), d => Distance.FromKilometers(d)) { }
+}
+public class DistanceToMilesConverter : ValueConverter<Distance, double>
+{
+    public DistanceToMilesConverter() : base(area => (double)area.ToMiles(), d => Distance.FromMiles(d)) { }
+}
