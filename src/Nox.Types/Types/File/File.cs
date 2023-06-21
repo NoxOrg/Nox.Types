f078287267aab3c1759836a4506adb962e0adf80
Ricardo Rocha <rochar@gmail.com>
2023-06-21 09:49:01 +0100

    create placeholder for nox.types to allow to be used on yaml solution files

---
+++ src/Nox.Types/Types/File/File.cs
@@ -0,0 +1,8 @@
+namespace Nox.Types;
+
+    /// <summary>
+    /// Represents a Nox <see cref="File"/> type and value object.
+    /// </summary>
+    public sealed class File : ValueObject<string, File>
+    {
+    }
---
+++ src/Nox.Types/Types/Password/Password.cs
@@ -0,0 +1,8 @@
+namespace Nox.Types;
+
+    /// <summary>
+    /// Represents a Nox <see cref="Password"/> type and value object.
+    /// </summary>
+    public sealed class Password : ValueObject<string, Password>
+    {
+    }
