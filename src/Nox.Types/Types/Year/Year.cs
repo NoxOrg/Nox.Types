3a27c4390e50d04a197a4f8bba6862d53228b955
Denis Vieira dos Anjos <denis.vieira@authoritypartners.com>
2023-06-25 21:55:06 -0300

    Year implementation

--- src/Nox.Types/Types/Year/Year.cs
+++ src/Nox.Types/Types/Year/Year.cs
@@ -1,9 +1,38 @@
 namespace Nox.Types;
 
+/// <summary>
+/// Represents a Nox <see cref="Year"/> type and value object.
+/// </summary>
+public sealed class Year : ValueObject<ushort, Year>
+{
     /// <summary>
-    /// Represents a Nox <see cref="Year"/> type and value object.
+    /// The minimum valid year value.
     /// </summary>
-    /// <remarks>Placeholder, needs to be implemented</remarks>
-    public sealed class Year : ValueObject<ushort, Year>
+    private const ushort MinYearValue = 1;
+
+    /// <summary>
+    /// The maximum valid year value.
+    /// </summary>
+    private const ushort MaxYearValue = 9999;
+
+    /// <summary>
+    /// Validates the <see cref="Year"/> object.
+    /// </summary>
+    /// <returns>A validation result indicating whether the <see cref="Year"/> object is valid or not.</returns>
+    internal override ValidationResult Validate()
     {
+        var result = base.Validate();
+
+        if (Value is < MinYearValue or > MaxYearValue)
+        {
+            result.Errors.Add(new ValidationFailure(nameof(Value), $"Could not create a Nox Year type with unsupported value '{Value}'. The value must be between {MinYearValue} and {MaxYearValue}."));
+        }
+        return result;
     }
+
+    /// <inheritdoc />
+    public override string ToString()
+    {
+        return Value.ToString("0000");
+    }
+}
--- tests/Nox.Types.Tests/Types/Year/YearTests.cs
+++ tests/Nox.Types.Tests/Types/Year/YearTests.cs
@@ -1,11 +1,65 @@
-﻿// ReSharper disable once CheckNamespace
-namespace Nox.Types.Tests.Types;
+﻿namespace Nox.Types.Tests.Types;
 
 public class YearTests
 {
     [Fact]
-    public void When_Create_Should()
+    public void Nox_Year_Constructor_ReturnsSameValue()
     {
+        var testYear = (ushort)1;
 
+        var number = Year.From(testYear);
+
+        Assert.Equal(testYear, number.Value);
+    }
+
+    [Theory]
+    [InlineData((ushort)0)]
+    [InlineData((ushort)(19999))]
+    public void Nox_Year_Constructor_WithOutOfRangeYear_ThrowsValidationException(byte value)
+    {
+        // Arrange & Act
+        var exception = Assert.Throws<TypeValidationException>(() => _ =
+          Year.From(value)
+        );
+
+        // Assert
+        Assert.Equal($"Could not create a Nox Year type with unsupported value '{value}'. The value must be between 1 and 9999.", exception.Errors.First().ErrorMessage);
+    }
+
+    [Fact]
+    public void Nox_Year_Equality_Tests()
+    {
+        // Arrange
+        var year1 = Year.From(1);
+
+        var year2 = Year.From(1);
+
+        // Assert
+        Assert.Equal(year1, year2);
+    }
+
+    [Fact]
+    public void Nox_Year_NotEqual_Tests()
+    {
+        // Arrange
+        var year1 = Year.From(1);
+
+        var year2 = Year.From(2);
+
+        // Assert
+        Assert.NotEqual(year1, year2);
+    }
+
+
+    [Fact]
+    public void Nox_Year_ToString_ReturnsString()
+    {
+        // Arrange
+        var year = Year.From(1);
+        var year2 = Year.From(199);
+
+        // Assert
+        Assert.Equal("0001", year.ToString());
+        Assert.Equal("0199", year2.ToString());
     }
 }
