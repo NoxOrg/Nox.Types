e96af20bf6d1d227dc5032fcacbb3556b2f208bb
Denis Vieira dos Anjos <denis.vieira@authoritypartners.com>
2023-06-28 18:05:10 -0300

    Changing method to be more readable

--- src/Nox.Types.EntityFramework/CurrencyCode3Converter.cs
+++ src/Nox.Types.EntityFramework/CurrencyCode3Converter.cs
@@ -10,6 +10,6 @@ public class CurrencyCode3Converter : ValueConverter<CurrencyCode3, string>
     /// <summary>
     /// Initializes a new instance of the <see cref="CurrencyCode3Converter" /> class.
     /// </summary>
-    public CurrencyCode3Converter() : base(currencyCode3 => currencyCode3.Value, currencyCode3 => CurrencyCode3.From(currencyCode3)) { }
+    public CurrencyCode3Converter() : base(currencyCode3 => currencyCode3.Value, currencyCode3Value => CurrencyCode3.From(currencyCode3Value)) { }
 }
 
