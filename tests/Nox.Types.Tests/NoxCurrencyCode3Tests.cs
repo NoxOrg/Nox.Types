bf5c40416195c157fa45b80f55dee7373f747c85
Denis Vieira dos Anjos <denis.vieira@authoritypartners.com>
2023-06-28 06:07:33 -0300

    Code review

--- tests/Nox.Types.Tests/NoxCurrencyCode3Tests.cs
+++ tests/Nox.Types.Tests/NoxCurrencyCode3Tests.cs
@@ -1,139 +1,11 @@
-﻿namespace Nox.Types.Tests;
+﻿using Nox.Types.Tests.Types;
+
+namespace Nox.Types.Tests;
 public class NoxCurrencyCode3Tests
 {
-    [InlineData("AED")]
-    [InlineData("AFN")]
-    [InlineData("ALL")]
-    [InlineData("AMD")]
-    [InlineData("ANG")]
-    [InlineData("AOA")]
-    [InlineData("ARS")]
-    [InlineData("AUD")]
-    [InlineData("AWG")]
-    [InlineData("AZN")]
-    [InlineData("BAM")]
-    [InlineData("BBD")]
-    [InlineData("BDT")]
-    [InlineData("BGN")]
-    [InlineData("BHD")]
-    [InlineData("BIF")]
-    [InlineData("BMD")]
-    [InlineData("BND")]
-    [InlineData("BOB")]
-    [InlineData("BRL")]
-    [InlineData("BSD")]
-    [InlineData("BTC")]
-    [InlineData("BTN")]
-    [InlineData("BWP")]
-    [InlineData("BYR")]
-    [InlineData("BYN")]
-    [InlineData("BZD")]
-    [InlineData("CAD")]
-    [InlineData("CDF")]
-    [InlineData("CHF")]
-    [InlineData("CLP")]
-    [InlineData("CNY")]
-    [InlineData("COP")]
-    [InlineData("CRC")]
-    [InlineData("CUC")]
-    [InlineData("CUP")]
-    [InlineData("CVE")]
-    [InlineData("CZK")]
-    [InlineData("DJF")]
-    [InlineData("DKK")]
-    [InlineData("DOP")]
-    [InlineData("DZD")]
-    [InlineData("EGP")]
-    [InlineData("ERN")]
-    [InlineData("ETB")]
-    [InlineData("EUR")]
-    [InlineData("FJD")]
-    [InlineData("FKP")]
-    [InlineData("GBP")]
-    [InlineData("GEL")]
-    [InlineData("GHS")]
-    [InlineData("GIP")]
-    [InlineData("GMD")]
-    [InlineData("GNF")]
-    [InlineData("GTQ")]
-    [InlineData("GYD")]
-    [InlineData("HKD")]
-    [InlineData("HNL")]
-    [InlineData("HRK")]
-    [InlineData("HTG")]
-    [InlineData("HUF")]
-    [InlineData("IDR")]
-    [InlineData("ILS")]
-    [InlineData("INR")]
-    [InlineData("IQD")]
-    [InlineData("IRR")]
-    [InlineData("ISK")]
-    [InlineData("JMD")]
-    [InlineData("JOD")]
-    [InlineData("JPY")]
-    [InlineData("KES")]
-    [InlineData("KGS")]
-    [InlineData("KHR")]
-    [InlineData("KMF")]
-    [InlineData("KPW")]
-    [InlineData("KRW")]
-    [InlineData("KWD")]
-    [InlineData("KYD")]
-    [InlineData("KZT")]
-    [InlineData("LAK")]
-    [InlineData("LBP")]
-    [InlineData("LKR")]
-    [InlineData("LRD")]
-    [InlineData("LSL")]
-    [InlineData("LYD")]
-    [InlineData("MAD")]
-    [InlineData("MDL")]
-    [InlineData("MGA")]
-    [InlineData("MKD")]
-    [InlineData("MMK")]
-    [InlineData("MNT")]
-    [InlineData("MOP")]
-    [InlineData("MRO")]
-    [InlineData("MTL")]
-    [InlineData("MUR")]
-    [InlineData("MVR")]
-    [InlineData("MWK")]
-    [InlineData("MXN")]
-    [InlineData("MYR")]
-    [InlineData("MZN")]
-    [InlineData("NAD")]
-    [InlineData("NGN")]
-    [InlineData("NIO")]
-    [InlineData("NOK")]
-    [InlineData("NPR")]
-    [InlineData("NZD")]
-    [InlineData("OMR")]
-    [InlineData("PAB")]
-    [InlineData("PEN")]
-    [InlineData("PGK")]
-    [InlineData("PHP")]
-    [InlineData("PKR")]
-    [InlineData("PLN")]
-    [InlineData("PYG")]
-    [InlineData("QAR")]
-    [InlineData("RON")]
-    [InlineData("RSD")]
-    [InlineData("RUB")]
-    [InlineData("RWF")]
-    [InlineData("SAR")]
-    [InlineData("SBD")]
-    [InlineData("SCR")]
-    [InlineData("SDD")]
-    [InlineData("SDG")]
-    [InlineData("SEK")]
-    [InlineData("SGD")]
-    [InlineData("SHP")]
-    [InlineData("SLL")]
-    [InlineData("SOS")]
-    [InlineData("SRD")]
-    [InlineData("STD")]
     [Theory]
-    public void Nox_CurrencyCode3_Constructor_ReturnsSameValue_AllCurrencies(string currencyCode3String)
+    [ClassData(typeof(CurrencyCodeData))]
+    public void CurrencyCode3_Constructor_ReturnsSameValue_AllCurrencies(string currencyCode3String)
     {
         var currencyCode3 = CurrencyCode3.From(currencyCode3String);
 
@@ -141,7 +13,7 @@ public class NoxCurrencyCode3Tests
     }
 
     [Fact]
-    public void Nox_CurrencyCode3_Constructor_WithUnsupportedCurrencyCode3_ThrowsValidationException()
+    public void CurrencyCode3_Constructor_WithUnsupportedCurrencyCode3_ThrowsValidationException()
     {
         var exception = Assert.Throws<TypeValidationException>(() => _ =
           CurrencyCode3.From("ABC")
@@ -151,7 +23,7 @@ public class NoxCurrencyCode3Tests
     }
 
     [Fact]
-    public void Nox_CurrencyCode3_Equality_Tests()
+    public void CurrencyCode3_Equality_Tests()
     {
         var currencyCode3_1 = CurrencyCode3.From("USD");
 
@@ -161,7 +33,7 @@ public class NoxCurrencyCode3Tests
     }
 
     [Fact]
-    public void Nox_CurrencyCode3_NotEqual_Tests()
+    public void CurrencyCode3_NotEqual_Tests()
     {
         var currencyCode3_1 = CurrencyCode3.From("RWF");
 
@@ -171,7 +43,7 @@ public class NoxCurrencyCode3Tests
     }
 
     [Fact]
-    public void Nox_CurrencyCode3_ToString_ReturnsString()
+    public void CurrencyCode3_ToString_ReturnsString()
     {
         var currencyCode3 = CurrencyCode3.From("USD");
 
