c7ac4c52d22ed0c7f81e1c4e643f5fdab8ebe8d8
Denis Vieira dos Anjos <denis.vieira@authoritypartners.com>
2023-06-28 07:03:22 -0300

    Percentagem implementation

--- src/Nox.Types/Types/Percentage/Percentage.cs
+++ src/Nox.Types/Types/Percentage/Percentage.cs
@@ -1,9 +1,39 @@
+using System;
+
 namespace Nox.Types;
 
+/// <summary>
+/// Represents a Nox <see cref="Percentage"/> type and value object.
+/// </summary>
+public sealed class Percentage : ValueObject<float, Percentage>
+{
+    private readonly PercentageTypeOptions _percentageTypeOptions = new();
+
     /// <summary>
-    /// Represents a Nox <see cref="Percentage"/> type and value object.
+    /// Validates a <see cref="Percentage"/> object.
     /// </summary>
-    /// <remarks>Placeholder, needs to be implemented</remarks>
-    public sealed class Percentage : ValueObject<float, Percentage>
+    /// <returns>true if the <see cref="Percentage"/> value is valid.</returns>
+    internal override ValidationResult Validate()
     {
+        var result = base.Validate();
+
+        if (Value < _percentageTypeOptions.MinValue)
+        {
+            result.Errors.Add(new ValidationFailure(nameof(Value), $"Could not create a Nox Percentage type as value {Value} is less than than the minimum specified value of {_percentageTypeOptions.MinValue}"));
+        }
+
+        if (Value > _percentageTypeOptions.MaxValue)
+        {
+            result.Errors.Add(new ValidationFailure(nameof(Value), $"Could not create a Nox Percentage type a value {Value} is greater than than the maximum specified value of {_percentageTypeOptions.MaxValue}"));
+        }
+
+        Value = (float)Math.Round(Value, _percentageTypeOptions.Digits);
+
+        return result;
     }
+
+    public override string ToString()
+    {
+        return $"{Value * 100}%";
+    }
+}
--- tests/Nox.Types.Tests/Types/Percentage/PercentageTests.cs
+++ tests/Nox.Types.Tests/Types/Percentage/PercentageTests.cs
@@ -1,11 +1,61 @@
-﻿// ReSharper disable once CheckNamespace
-namespace Nox.Types.Tests.Types;
+﻿namespace Nox.Types.Tests.Types;
 
 public class PercentageTests
 {
     [Fact]
-    public void When_Create_Should()
+    public void Percentage_Constructor_ReturnsSameValue()
     {
+        var testPercentage = 0.5f;
 
+        var number = Percentage.From(testPercentage);
+
+        Assert.Equal(testPercentage, number.Value);
+    }
+
+    [Fact]
+    public void Percentage_Constructor_ThrowsException_WhenValueExceedsMaxAllowed()
+    {
+        var testPercentage = 3.2f;
+
+        Assert.Throws<TypeValidationException>(() => _ =
+            Percentage.From(testPercentage)
+        );
+    }
+
+    [Fact]
+    public void Percentage_Constructor_ThrowsException_WhenValueIsLessThanMinAllowed()
+    {
+        var testPercentage = -0.3f;
+
+        Assert.Throws<TypeValidationException>(() => _ =
+            Percentage.From(testPercentage)
+        );
+    }
+
+    [Fact]
+    public void Percentage_Constructor_RoundsFloatValues_WhenConstructedWithFloatInput()
+    {
+        var testPercentage = 0.4f;
+
+        var percentage = Number.From(testPercentage);
+
+        Assert.Equal(40, percentage.Value);
+    }
+
+    [Fact]
+    public void Percentage_ToString_Returns_Value()
+    {
+        void Test()
+        {
+            var pecentageValue = 0.45f;
+
+            var percentage = Number.From(pecentageValue);
+
+            var percentageAsString = percentage.ToString();
+
+            Assert.Equal("45%", percentageAsString);
+        }
+
+        TestUtility.RunInInvariantCulture(Test);
     }
 }
