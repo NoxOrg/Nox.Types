ed7ff0e243303f9bc00feb7dbd260d21964a4193
Denis Vieira dos Anjos <denis.vieira@authoritypartners.com>
2023-06-28 08:58:03 -0300

    Currency data

--- tests/Nox.Types.Tests/CurrencyCodeData.cs
+++ tests/Nox.Types.Tests/CurrencyCodeData.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections;
 
 namespace Nox.Types.Tests.Types;
 public class CurrencyCodeData : IEnumerable<object[]>
