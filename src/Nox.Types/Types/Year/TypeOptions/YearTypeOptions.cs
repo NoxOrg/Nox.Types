1ec5cffe2025ed9893a57361692850f8933eb75c
Denis Vieira dos Anjos <denis@Deniss-MacBook-Pro.local>
2023-06-30 08:05:00 -0300

    Name consistent

--- src/Nox.Types/Types/Year/TypeOptions/YearTypeOptions.cs
+++ src/Nox.Types/Types/Year/TypeOptions/YearTypeOptions.cs
@@ -4,8 +4,8 @@ namespace Nox.Types;
 public class YearTypeOptions
 {
     public static readonly ushort DefaultMinValue= 1900;
-    public static readonly ushort MaxYearValue = 3000;
-    public ushort MinValue { get; set; } = MinYearValue;
-    public ushort MaxValue { get; set; } = MaxYearValue;
+    public static readonly ushort DefaultMaxValue = 3000;
+    public ushort MinValue { get; set; } = DefaultMinValue;
+    public ushort MaxValue { get; set; } = DefaultMaxValue;
     public bool AllowFutureOnly { get; set; } = false;
 }
