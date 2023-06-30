6e9bd808e4c60ad0d2ec094440324b25f552b972
Denis Vieira <137417381+denisvieira-dev@users.noreply.github.com>
2023-06-30 07:58:01 -0300

    Update src/Nox.Types/Types/Year/TypeOptions/YearTypeOptions.cs
    Co-authored-by: Ricardo Rocha <rochar@users.noreply.github.com>

--- src/Nox.Types/Types/Year/TypeOptions/YearTypeOptions.cs
+++ src/Nox.Types/Types/Year/TypeOptions/YearTypeOptions.cs
@@ -3,7 +3,7 @@ namespace Nox.Types;
 
 public class YearTypeOptions
 {
-    public static readonly ushort MinYearValue = 1900;
+    public static readonly ushort DefaultMinValue= 1900;
     public static readonly ushort MaxYearValue = 3000;
     public ushort MinValue { get; set; } = MinYearValue;
     public ushort MaxValue { get; set; } = MaxYearValue;
