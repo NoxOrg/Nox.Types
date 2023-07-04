3b397f09f7ceee3b98bce94d46da252ed2b5dd68
Dejan Bratic <dejan.bratic@igwplc.com>
2023-07-04 11:20:57 +0200

    initial implementation;

---
+++ src/Nox.Types/Types/JwtToken/JwtToken.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nox.Types;
+
+public sealed class JwtToken : ValueObject<string, JwtToken>
+{
+    private const char JWTPartsDelimiter = '.';
+    private const int NumberOfJwtParts = 3;
+
+    private const int JWTHeaderPart = 0;
+    private const int JWTPayloadPart = 1;
+    private const int JWTSignaturePart = 2;
+
+    /// <summary>
+    /// Validates a <see cref="JwtToken"/> object.
+    /// </summary>
+    /// <returns>true if the <see cref="JwtToken"/> value is valid.</returns>
+    internal override ValidationResult Validate()
+    {
+        var result = base.Validate();
+
+        if(string.IsNullOrEmpty(Value))
+        {
+            result.Errors.Add(new ValidationFailure(nameof(Value), "Could not create a Nox JWT Token type as the value cannot be null or empty string."));
+        }
+
+        else if(DoesNotHaveValidJWTFormat(Value))
+        {
+            result.Errors.Add(new ValidationFailure(nameof(Value), $"Could not create a Nox JWT Token type as value {Value} does not have a valid JWT format."));
+        }
+
+        return result;
+    }
+
+    private bool DoesNotHaveValidJWTFormat(string value)
+    {
+        var jwtParts = value.Split(JWTPartsDelimiter);
+
+        if (jwtParts.Length != NumberOfJwtParts)
+            return true;
+
+        var header = jwtParts[JWTHeaderPart];
+        var payload = jwtParts[JWTPayloadPart];
+        var signature = jwtParts[JWTSignaturePart];
+
+        return false;
+    }
+}
---
+++ tests/Nox.Types.Tests/Types/JwtToken/JwtTokenTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+
+namespace Nox.Types.Tests.Types;
+
+public class JwtTokenTests
+{
+    [Fact]
+    public void JwtToken_Constructor_ReturnsSameValue()
+    {
+        var value = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+
+        var jwtToken = JwtToken.From(value);
+
+        Assert.Equal(value, jwtToken.Value);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    public void JwtToken_Constructor_WithNullOrEmptyStringValue_ThrowsException(string value)
+    {
+        var action = () => JwtToken.From(value);
+
+        action.Should().Throw<TypeValidationException>()
+            .And.Errors.Should().BeEquivalentTo(new[] { new ValidationFailure("Value", "Could not create a Nox JWT Token type as the value cannot be null or empty string.") });
+    }
+
+    [Theory]
+    [InlineData("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ")] // missing last segment
+    public void JwtToken_Constructor_WithInvalidJWTFormatValue_ThrowsException(string value)
+    {
+        var action = () => JwtToken.From(value);
+
+        action.Should().Throw<TypeValidationException>()
+            .And.Errors.Should().BeEquivalentTo(new[] { new ValidationFailure("Value", $"Could not create a Nox JWT Token type as value {value} does not have a valid JWT format.") });
+    }
+}
