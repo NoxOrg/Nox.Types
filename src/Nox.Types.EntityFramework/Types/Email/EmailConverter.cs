aa4c8873d333c9d6b2dcf0591ca2e8b486a9f64e
Dejan Bratic <dejan.bratic@igwplc.com>
2023-07-04 15:53:17 +0200

    added missing converter;

---
+++ src/Nox.Types.EntityFramework/Types/Email/EmailConverter.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Nox.Types.EntityFramework.Types;
+
+public class EmailConverter : ValueConverter<Email, string>
+{
+    public EmailConverter() : base(email => email.Value, emailValue => Email.From(emailValue)) { }
+}
